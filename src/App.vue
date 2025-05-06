fa2358f5f801361a4db13b9ea729a877c343b584
[saodial hasan] <saodialhasan@gmail.com>
2025-05-06 11:35:07 +0600

    Add Vue project files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <div class="mainDisplay max-w-312.5 m-auto">
+   
+<Navbar/>
+  </div>
+  np
+</template>
+
+<style scoped>
+
+</style>
